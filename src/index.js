295edb9a36386520919eccca24689d27d07913d0
Oleksandr Balitskiy <popingalov@gmail.com>
2021-11-15 01:36:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -26,3 +26,24 @@ for (let e = 0; e < arr.length / 2; e++) {
 }
 arr.length = arr.length / 2;
 console.log(arr);
+/* const test = [1, 1, 2, 2, 3, 3, 4, 6, 9, 9];
+const test2 = () => {
+  for (let i = 0; i < test.length; i += 1) {
+    console.log(test[i]);
+    let a = test[i];
+    let b = test[i + 1];
+    if ((a ^= test[i]) === 1) {
+      return test[i];
+    }
+  }
+};
+
+console.log(test, test2()); */
+function testXor(numbers) {
+  return numbers.reduce((acc, e) => {
+    console.log(acc, e);
+    return acc ^ e;
+  });
+}
+const test = [1, 2, 2, 2, 3, 3, 8, 9, 9, 1, 1, 1, 2];
+console.log(testXor(test));
