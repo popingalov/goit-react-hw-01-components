b62a4ea035c5521facb7fbb25111a18bbd59faca
Oleksandr Balitskiy <popingalov@gmail.com>
2021-11-17 22:33:58 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,3 @@
-import paintings from './paintings.json';
-import PaintingList from './components/Painting/PaintingList';
-import Section from './components/Section';
-import ColorPicker from 'components/ColorPicker/ColorPicker';
-import Alert from './components/Alert/Alert';
-import Container from 'components/Container/Container';
-import Box from './components/Box/Box';
-const colorPickerOptions = [
-  { label: 'red', color: '#F44336' },
-  { label: 'green', color: '#4CAF50' },
-  { label: 'blue', color: '#2196F3' },
-  { label: 'grey', color: '#607D8B' },
-  { label: 'pink', color: '#E91E63' },
-  { label: 'indigo', color: '#3F51B5' },
-];
 export default function App() {
-  return (
-    <Container>
-      <Section title="топ продаж">
-        <PaintingList items={paintings} />
-      </Section>
-      <Alert text="Шеф все пропало!" type="success" />
-      <Alert text="Шеф все пропало!" type="warning" />
-      <Alert text="Шеф все пропало!" type="error" />
-      <ColorPicker options={colorPickerOptions} />
-      <ColorPicker options={colorPickerOptions} />
-      <Section title="Hi man" />
-      <Box type="small" classNames="big red" styles={{ color: '#fff' }} />
-      <Box type="medium" />
-      <Box type="large" />
-    </Container>
-  );
+  return <div>HI</div>;
 }
